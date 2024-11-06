bd446ed3d07422c11985a60b224a6da29909964c
Fausto Castaneda <fausto.castaneda.v@gmail.com>
2024-11-06 16:42:20 -0500

    Remove announcements, fix config and updates description

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   site: "https://faustocv.github.io",
-  base: 'lunaz-site'
+  base: 'lunaz-site',
 
   output: 'static',
 
